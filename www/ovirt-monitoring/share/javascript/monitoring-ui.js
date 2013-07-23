8f5afee4c3dcceb691bd0b302ac515f73867c9ff
r.koch <r.koch@ovido.at>
2013-07-23 21:47:36 +0200

    .

---
+++ www/ovirt-monitoring/share/javascript/monitoring-ui.js
@@ -0,0 +1,36 @@
+// This file is part of ovirt-Monitoring UI-Plugin.
+//
+// ovirt-Monitoring UI-Plugin is free software: you can redistribute it 
+// and/or modify it under the terms of the GNU General Public License 
+// as published by the Free Software Foundation, either version 3 of the i
+// License, or (at your option) any later version.
+//
+// ovirt-Monitoring UI-Plugin is distributed in the hope that it will be 
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ovirt-Monitoring UI-Plugin.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+
+// clickable table rows in monitoring results
+$(document).ready(function() {
+
+  // get JSON data
+  getResults();
+  //setInterval("getResults()", refreshInterval);
+  setInterval("getResults()", 30000);
+	
+});
+
+
+// get service stati for selected host/vm
+function getResults(){
+  // get hostname
+  var hostName = $("div[id='service-details']").attr("host");
+  
+}
+
+
