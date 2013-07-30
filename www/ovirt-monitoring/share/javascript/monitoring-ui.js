f9f71820e247136447bfe25f7edef0b1808d037b
Rene Koch <r.koch@ovido.at>
2013-07-30 14:11:59 +0200

    tabs for details and graphs

--- www/ovirt-monitoring/share/javascript/monitoring-ui.js
+++ www/ovirt-monitoring/share/javascript/monitoring-ui.js
@@ -22,7 +22,21 @@ $(document).ready(function() {
   getResults();
   //setInterval("getResults()", refreshInterval);
   setInterval("getResults()", 30000);
-	
+  
+  
+  // show tabs
+  $(function() {
+	$( "#tabs" ).tabs({
+	  beforeLoad: function( event, ui ) {
+		ui.jqXHR.error(function() {
+		  ui.panel.html(
+		    "Couldn't load tabs. " );
+		});
+	  }
+	});
+  });
+  
+  
 });
 
 
