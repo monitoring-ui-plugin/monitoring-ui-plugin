8f124fa2770f109db97ee6c0e6d078c6948ba73e
Rene Koch <r.koch@ovido.at>
2013-08-15 21:17:08 +0200

    copyright

--- lib/oVirtUI/Web.pm
+++ lib/oVirtUI/Web.pm
@@ -1,20 +1,27 @@
 #!/usr/bin/perl
 
-# This file is part of ovirt-Monitoring UI-Plugin.
+# COPYRIGHT:
 #
-# ovirt-Monitoring UI-Plugin is free software: you can redistribute it 
-# and/or modify it under the terms of the GNU General Public License 
-# as published by the Free Software Foundation, either version 3 of the i
-# License, or (at your option) any later version.
+# This software is Copyright (c) 2013 by René Koch
+#                             <r.koch@ovido.at>
 #
-# ovirt-Monitoring UI-Plugin is distributed in the hope that it will be 
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# This file is part of Monitoring UI-Plugin.
+#
+# (Except where explicitly superseded by other copyright notices)
+# Monitoring UI-Plugin is free software: you can redistribute it 
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of 
+# the License, or any later version.
+#
+# Monitoring UI-plugin is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ovirt-Monitoring UI-Plugin.  
+# along with Monitoring UI-Plugin.  
 # If not, see <http://www.gnu.org/licenses/>.
+
 
 package oVirtUI::Web;
 
